25445a7455bfa2c3f3ff9c7f72bb40101f3814cc
BryceKopan <BryceKopan@gmail.com>
2019-02-05 14:57:47 -0600

    Initial Inventory Initial Commit

---
+++ SoulHorizons/Assets/Scripts/Inventory/InitialInventory.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Encounter", menuName = "test")]
+public class InitialInventory : ScriptableObject 
+{
+    private static List<CardData> StartingInventory;
+}
