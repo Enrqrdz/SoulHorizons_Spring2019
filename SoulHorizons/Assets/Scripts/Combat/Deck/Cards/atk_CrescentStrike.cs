55db2a9c42a6893475c9257831125d590821aa46
Antonio Herrera <37684203+Azureknight117@users.noreply.github.com>
2019-03-02 11:35:33 -0600

    Finished coding out Crescent strike

---
+++ SoulHorizons/Assets/Scripts/Combat/Deck/Cards/atk_CrescentStrike.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Attacks/CrescentStrike")]
+[RequireComponent(typeof(AudioSource))]
+
+public class atk_CrescentStrike : AttackData
+{
+    private AudioSource PlayCardSFX;
+    public AudioClip CrescentSFX;
+    int playerX;
+    int playerY;
+
+    private void OnEnable()
+    {
+         playerX = GameObject.FindGameObjectWithTag("Player").GetComponent<scr_Entity>()._gridPos.x;
+         playerY = GameObject.FindGameObjectWithTag("Player").GetComponent<scr_Entity>()._gridPos.y;
+    }
+    public override Vector2Int BeginAttack(int xPos, int yPos, ActiveAttack activeAtk)
+    {
+        return new Vector2Int(xPos, yPos);
+    }
+    public override ActiveAttack BeginAttack(ActiveAttack activeAtk)
+    {
+        activeAtk.particle = Instantiate(particles, scr_Grid.GridController.GetWorldLocation(activeAtk.position), Quaternion.identity);
+        PlayCardSFX = GameObject.Find("DeckManager").GetComponent<AudioSource>();
+        PlayCardSFX.clip = CrescentSFX;
+        PlayCardSFX.Play();
+        activeAtk.attack.maxIncrementRange = ((scr_Grid.GridController.columnSizeMax * 2) + scr_Grid.GridController.rowSizeMax - 2);
+        return activeAtk;
+    }
+
+    public override bool CheckCondition(scr_Entity _ent)
+    {
+        return false;
+    }
+
+    public override void EndEffects(ActiveAttack activeAttack)
+    {
+
+    }
+
+    public override void ImpactEffects(int xPos = -1, int yPos = -1)
+    {
+    }
+
+    public override void LaunchEffects(ActiveAttack activeAttack)
+    {
+
+    }
+
+    public override Vector2Int ProgressAttack(int xPos, int yPos, ActiveAttack activeAtk)
+    {
+        int tempVarX = xPos - playerX;
+        int tempVarY = yPos - playerY;
+        if (tempVarX < 2)
+        {
+            return new Vector2Int(xPos++, yPos);
+        }
+        else if (tempVarX == 2 && yPos >= playerY)
+        {
+            return new Vector2Int(xPos, yPos--);
+        }
+        else
+        {
+            return new Vector2Int(xPos, yPos--);
+        }
+    }
+
+    public override void ProgressEffects(ActiveAttack activeAttack)
+    {
+        activeAttack.particle.transform.position = Vector3.Lerp(activeAttack.particle.transform.position, scr_Grid.GridController.GetWorldLocation(activeAttack.lastPosition.x, activeAttack.lastPosition.y) + activeAttack.attack.particlesOffset, (particleSpeed) * Time.deltaTime);
+        activeAttack.particle.transform.Rotate(0, 0, 30, Space.Self);
+        activeAttack.particle.sortingOrder = -activeAttack.position.y;
+    }
+}
+
---
+++ SoulHorizons/Assets/Scripts/Combat/Deck/Cards/scr_CrescentStrike.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+[CreateAssetMenu(menuName = "Cards/CrescentStrike")]
+public class scr_CrescentStrike : CardData
+{
+    public AttackData crescentAttack;
+    private AudioSource PlayCardSFX;
+    public AudioClip BoomerangSFX;
+
+    public override void Activate()
+    {
+
+        ActivateEffects();
+        PlayCardSFX = GameObject.Find("DeckManager").GetComponent<AudioSource>();
+        PlayCardSFX.clip = BoomerangSFX;
+        PlayCardSFX.Play();
+
+        scr_Entity player = GameObject.FindGameObjectWithTag("Player").GetComponent<scr_Entity>();
+
+        //add attack to attack controller script
+        //does a check to see if the target col is off the map
+        if (player._gridPos.y == scr_Grid.GridController.rowSizeMax - 1)
+        {
+            scr_AttackController.attackController.AddNewAttack(crescentAttack, player._gridPos.x + 2, player._gridPos.y, player);
+        }
+        else
+        {
+            scr_AttackController.attackController.AddNewAttack(crescentAttack, player._gridPos.x + 1, player._gridPos.y + 1, player);
+        }
+    }
+
+    public override void StartCastingEffects()
+    {
+
+    }
+
+    protected override void ActivateEffects()
+    {
+        //put start effects here
+    }
+}
