178866e8ca8853695f0c2e4da4ac59e7a34aeda7
Spencer Reynolds <sreynolds.jvhs@gmail.com>
2019-04-02 00:24:24 -0500

    Med Circle Animation Implemented

--- SoulHorizons/Assets/Scripts/Combat/Actions/Cards/scr_MeditationCircle.cs
+++ SoulHorizons/Assets/Scripts/Combat/Actions/Cards/scr_MeditationCircle.cs
@@ -9,6 +9,7 @@ public class scr_MeditationCircle : ActionData
 {
     private AudioSource PlayCardSFX;
     public AudioClip MeditationSFX;
+    public GameObject MedCircleAnim;
 
     public float activeDuration;
     public float damageMultiplier;
@@ -23,5 +24,7 @@ public class scr_MeditationCircle : ActionData
         Entity player = GameObject.FindGameObjectWithTag("Player").GetComponent<Entity>();
 
         scr_Grid.GridController.grid[player._gridPos.x, player._gridPos.y].BuffTile(activeDuration, damageMultiplier, damageReducer);
+        GameObject MedCircleAnimation = Instantiate(MedCircleAnim, new Vector2(player._gridPos.x-0.3f, player._gridPos.y-0.2f),Quaternion.identity);
+        Destroy(MedCircleAnimation, activeDuration);
     }
 }
