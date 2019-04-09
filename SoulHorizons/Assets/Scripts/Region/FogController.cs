01bf05e4485b06a5f9b1e9c6a3799f1ae842da75
BryceKopan <BryceKopan@gmail.com>
2019-04-09 03:19:42 -0500

    Fog Polish

--- SoulHorizons/Assets/Scripts/Region/FogController.cs
+++ SoulHorizons/Assets/Scripts/Region/FogController.cs
@@ -9,26 +9,42 @@ public class FogController : MonoBehaviour
     public Vector3 topLeftCorner, bottomRightCorner;
 
     [Header("Options")]
-    private int cloudNumber = 1000;
+    private int cloudsPerRow = 30;
+    private int numberOfRows = 30;
     private float maxForce = 10f;
+    private int minmumOrderInLayer = 2;
+    private float scaleRange = .2f;
 
-    private GameObject[] clouds;
+    private List<GameObject> clouds = new List<GameObject>();
 
     void Start()
     {
         Debug.Log("test");
-        clouds = new GameObject[cloudNumber];
 
-        for(int i = 0; i < clouds.Length; i++)
+        float verticalDistancePerRow = (topLeftCorner.y - bottomRightCorner.y) / numberOfRows;
+
+        for(int i = 0; i < numberOfRows; i++)
         {
-            int r = Random.Range(0, cloudPrefabs.Count);
-            Vector3 position = new Vector3(Random.Range(topLeftCorner.x, bottomRightCorner.x), Random.Range(topLeftCorner.y, bottomRightCorner.y), 0);
+            Vector3 force = new Vector3(Random.Range(-maxForce, maxForce), 0, 0);
 
-            clouds[i] = Instantiate(cloudPrefabs[r], position, Quaternion.identity);
+            for(int j = 0; j < cloudsPerRow; j++)
+            {
+                int r = Random.Range(0, cloudPrefabs.Count);
 
-            clouds[i].GetComponent<Rigidbody2D>().AddForce(new Vector3(Random.Range(-maxForce, maxForce), 0, 0));
+                float yPosition = (verticalDistancePerRow * i) + bottomRightCorner.y;
 
-            clouds[i].transform.parent = gameObject.transform;
+                Vector3 position = new Vector3(Random.Range(topLeftCorner.x, bottomRightCorner.x), yPosition, 0);
+
+                GameObject newCloud = Instantiate(cloudPrefabs[r], position, Quaternion.identity);
+
+                newCloud.GetComponent<Rigidbody2D>().AddForce(force);
+                newCloud.GetComponent<SpriteRenderer>().sortingOrder = numberOfRows + minmumOrderInLayer - i;
+                float rScale = Random.Range(1 - scaleRange, 1 + scaleRange);
+                newCloud.transform.localScale = new Vector3(rScale, rScale, 1);
+                newCloud.transform.parent = gameObject.transform;
+
+                clouds.Add(newCloud);
+            }
         }
     }
 
