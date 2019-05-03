697ac43d31bd4445f4778745b4ba724ced057bce
Enrique Rodriguez <rodriguezenrique009@gmail.com>
2019-05-03 14:09:36 -0500

    mlagents in

---
+++ SoulHorizons/Assets/Machine Learning/Scripts/MushineAgent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public enum AgentStates {MoveUp, MoveDown, Attacking, Idle}
+public class MushineAgent : Agent
+{
+    public override void CollectObservations()
+    {
+
+    }
+
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+
+    }
+
+    public override void AgentReset()
+    {
+
+    }
+
+    public override void AgentOnDone()
+    {
+
+    }
+}
