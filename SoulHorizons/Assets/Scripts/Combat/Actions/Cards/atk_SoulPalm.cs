740cc4fc95754cbebcc0fc475c72477019ef2e7e
Enrique Rodriguez <rodriguezenrique009@gmail.com>
2019-05-06 00:31:10 -0500

    New Soul Palm

--- SoulHorizons/Assets/Scripts/Combat/Actions/Cards/atk_SoulPalm.cs
+++ SoulHorizons/Assets/Scripts/Combat/Actions/Cards/atk_SoulPalm.cs
@@ -11,21 +11,28 @@ public class atk_SoulPalm : AttackData
     Entity player;
     int playerX;
     int playerY;
-    float verticalOffset = 1.5f;
+    public int currentIncrement;
+    public int tempDamage;
+    public float verticalOffset = 1.5f;
+    public float horizontalOffset = 1.5f;
     static int counter = 0;
     
-
     public override Vector2Int BeginAttack(int xPos, int yPos, ActiveAttack activeAtk)
     {
+        currentIncrement = 0;
+        damage = 0;
         return new Vector2Int(xPos, yPos);
     }
+
     public override ActiveAttack BeginAttack(ActiveAttack activeAtk)
     {
         counter++;
+        currentIncrement = 0;
+        damage = 0;
 
-        if (counter == 2)
+        if (counter == 1)
         {
-            activeAtk.particle = Instantiate(particles, scr_Grid.GridController.GetWorldLocation(playerX, playerY) + new Vector3(0, verticalOffset, 0), Quaternion.identity);
+            activeAtk.particle = Instantiate(particles, scr_Grid.GridController.GetWorldLocation(playerX + 1, playerY) + new Vector3(horizontalOffset, verticalOffset, 0), Quaternion.identity);
         }
 
         if(player == null)
@@ -53,6 +60,7 @@ public class atk_SoulPalm : AttackData
     public override void EndEffects(ActiveAttack activeAttack)
     {
         counter = 0;
+        currentIncrement = 0;
     }
 
     public override void ImpactEffects(int xPos = -1, int yPos = -1)
@@ -66,8 +74,22 @@ public class atk_SoulPalm : AttackData
 
     public override Vector2Int ProgressAttack(int xPos, int yPos, ActiveAttack activeAtk)
     {
-        return new Vector2Int(xPos + 1, yPos);
+        currentIncrement++;
+        currentIncrement %= (6 * (maxIncrementRange + 1)) + 1;
+        Debug.Log(currentIncrement);
+
+        if (currentIncrement > 6 * maxIncrementRange - 1)
+        {
+            Debug.Log("Current Increment matches Max Increment");
+            damage = tempDamage;
+        }
+        else
+        {
+            damage = 0;
+        }
+        return new Vector2Int(xPos, yPos);
     }
+
     public override void ProgressEffects(ActiveAttack activeAttack)
     {
         if (activeAttack.particle != null)
